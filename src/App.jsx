183ab8f98dd6341ed42b451306fc9089b2f0956a
akib1437 <akibtanzim8@gmail.com>
2024-10-26 15:05:36 +0600

    navbar and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
 import './App.css'
+import Navbar from './components/Navbar/Navbar.jsx';
+import Banner from './components/Banner/Banner';
+import { useState } from 'react';
 
-function App() {
-  
+const App = () => {
+  const [coins, setCoins] = useState(0);
 
-  
-}
+  const increaseCoins = () => {
+    setCoins(coins + 20000);
+  }
+  return (
+    <div>
+      <Navbar coins={coins}/>
+      <Banner increaseCoins={increaseCoins}/>
+    </div>
+  );
+};
 
 export default App
